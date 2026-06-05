436fcf3cc1805c90b4e585521bcf73f92d0cf750
Daniil Buravlev <burravlev@icloud.com>
2026-06-05 10:33:16 +0300

    move file_mgr test

--- file/src/mgr.rs
+++ file/src/mgr.rs
@@ -84,4 +84,29 @@ mod tests {
         let calculated = mgr.size("test").unwrap();
         assert_eq!(calculated, size as u64);
     }
+
+    #[test]
+    fn file_mgr() {
+        let file = tempdir().unwrap();
+        let mgr = FileMgr::new(file.path(), 4096).unwrap();
+        let block_id = BlockId::new("testfile", 2);
+        let mut p1 = Page::new(mgr.block_size());
+        let pos1 = 88;
+
+        let str_value = "abcdefghjklm";
+
+        p1.set_string(pos1, String::from(str_value));
+        let size = Page::max_length(str_value);
+
+        let i32_value = 345;
+
+        let pos2 = pos1 + size;
+        p1.set_i32(pos2, 345);
+        mgr.write(block_id.clone(), p1).unwrap();
+
+        let p2 = mgr.read(block_id).unwrap();
+
+        assert_eq!(p2.get_i32(pos2), i32_value);
+        assert_eq!(p2.get_string(pos1, str_value.len()), str_value);
+    }
 }
