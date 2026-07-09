add8de37f565541abdcfdc840bf712719bd8776b
github-action-benchmark <github@users.noreply.github.com>
2026-07-09 20:48:48 +0000

    add Benchmark (cargo) benchmark result for 475f6beea4466b3e26f2111412152bbd270556eb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783166302687,
+  "lastUpdate": 1783630128601,
   "repoUrl": "https://github.com/daniilburavlev/simpledb",
   "entries": {
     "Benchmark": [
@@ -37,6 +37,48 @@ window.BENCHMARK_DATA = {
             "name": "index_insert",
             "value": 248303,
             "range": "± 32725",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "burravlev@icloud.com",
+            "name": "Daniil Buravlev",
+            "username": "daniilburavlev"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "475f6beea4466b3e26f2111412152bbd270556eb",
+          "message": "Add benchmarks checks (#6)\n\n* Finish network server layer\n\n* Fix network, fix bench action\n\n* Fix CI bench",
+          "timestamp": "2026-07-09T23:47:17+03:00",
+          "tree_id": "57cd0f3bff099c2b37dfe5a9e025970423445fcd",
+          "url": "https://github.com/daniilburavlev/simpledb/commit/475f6beea4466b3e26f2111412152bbd270556eb"
+        },
+        "date": 1783630128032,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "insert",
+            "value": 251931,
+            "range": "± 37637",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "join",
+            "value": 10493719,
+            "range": "± 148265",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "index_insert",
+            "value": 275205,
+            "range": "± 35376",
             "unit": "ns/iter"
           }
         ]
